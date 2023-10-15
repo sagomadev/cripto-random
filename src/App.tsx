5d2a7a3527a2a7e44ba9733ee5ed00b55522cb68
Samuel Gómez <sagomadev@gmail.com>
2023-10-15 09:24:56 +0200

    Refactor code to use Tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useReducer, useState } from "react";
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
 const getRandomNumberFromApi = async (): Promise<number> => {
   const res = await fetch(
@@ -12,34 +12,20 @@ const getRandomNumberFromApi = async (): Promise<number> => {
 };
 
 export const App = () => {
-  const [number, setNumber] = useState<number>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>();
-  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
-
-  useEffect(() => {
-    setIsLoading(true);
-    getRandomNumberFromApi()
-      .then(setNumber)
-      .catch((error) => setError(error.message));
-  }, [key]);
-
-  useEffect(() => {
-    if (number) setIsLoading(false);
-  }, [number]);
-
-  useEffect(() => {
-    if (error) setIsLoading(false);
-  }, [error]);
+  const query = useQuery(["randomNumber"], getRandomNumberFromApi);
 
   return (
     <div className="App App-header">
-      {isLoading ? <h2>Cargando...</h2> : <h2>Número aleatorio: {number} </h2>}
+      {query.isFetching ? (
+        <h2>Cargando...</h2>
+      ) : (
+        <h2>Número aleatorio: {query.data} </h2>
+      )}
 
-      {!isLoading && error && <h3>{error}</h3>}
+      {!query.isFetching && query.isError && <h3>{`${query.error}`}</h3>}
 
-      <button onClick={forceRefetch} disabled={isLoading}>
-        {isLoading ? "..." : "Nuevo número"}
+      <button onClick={() => query.refetch()} disabled={query.isFetching}>
+        {query.isFetching ? "..." : "Nuevo número"}
       </button>
     </div>
   );
